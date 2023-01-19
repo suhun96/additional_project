4035b61c692b52bc63f3e2651debd2e5fec7ee9a
김수훈 <sh_awtylol4@gimsuhun-ui-MacBookAir.local>
2023-01-19 18:26:15 +0900

    Hello people

--- user/views.py
+++ user/views.py
@@ -3,6 +3,10 @@ from django.views       import View
 from django.http        import JsonResponse
 # Create your views here.
 
-class HelloView(View):
+class HelloView1(View):
     def get(self, request):
-        return JsonResponse({'message' : 'hello yamuzin peole!'})
+        return JsonResponse({'message' : '승훈님 기대하겠습니다.'})
+
+class HelloView2(View):
+    def get(self, request):
+        return JsonResponse({'message' : '민기님 화이팅!'})
