f07dc036b6755c27c33ab5b47da115bd1ffec04c
김수훈 <sh_awtylol4@gimsuhun-ui-MacBookAir.local>
2023-01-20 11:35:14 +0900

    1월 20일 Iamport : GetAcessTokenView 작성 / 테스트 완료

---
+++ payment/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+
+from payment.views import *
+
+urlpatterns = [
+    path('resi',PaymentInfoResisterView.as_view()),
+    path('check',PaymentInfoCheckView.as_view()),
+    path('token',GetAcessTokenView.as_view())
+]
